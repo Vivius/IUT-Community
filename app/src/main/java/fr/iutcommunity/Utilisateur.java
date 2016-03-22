55590556a8a182fe7a33e0108e6811e8bf720bb3
Vivius <peillex.vincent@live.fr>
2016-03-22 14:46:30 +0100

    Classe utilisateur

---
+++ app/src/main/java/fr/iutcommunity/Utilisateur.java
@@ -0,0 +1,12 @@
+package fr.iutcommunity;
+
+/**
+ * Created by peillexv on 22/03/2016.
+ */
+public class Utilisateur {
+    private String login;
+    private String prenom;
+    private String nom;
+    private String password;
+    private String mail;
+}
