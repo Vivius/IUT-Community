79bcfd3eb791eba4d9ab765ed3e77af5699508cd
Vivius <peillex.vincentlive.fr>
2016-03-29 10:31:04 +0200

    En cours de développement

--- app/src/main/java/fr/iutcommunity/SendMessageActivity.java
+++ app/src/main/java/fr/iutcommunity/SendMessageActivity.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioButton;
@@ -34,6 +36,7 @@ public class SendMessageActivity extends AppCompatActivity implements ListView.O
     private static int HTTP_REQUEST_PROMOTIONS = 1;
     private static int HTTP_REQUEST_GROUPES = 2;
     private int httpRequest;
+    private LinearLayout llGroupes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,15 +46,18 @@ public class SendMessageActivity extends AppCompatActivity implements ListView.O
         rdgPromotions = (ViewGroup)findViewById(R.id.rdgPromotions);
         ddlDepartements = (Spinner)findViewById(R.id.ddlDepartements);
         ddlDepartements.setOnItemSelectedListener(this);
+
+        llGroupes = (LinearLayout)findViewById(R.id.llGroupes);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         rdgPromotions.setVisibility(View.VISIBLE);
-        httpRequest = HTTP_REQUEST_PROMOTIONS;
-        HashMap<String, String> departement = new HashMap<>();
         Log.d("Departement", ddlDepartements.getSelectedItem().toString());
+
+        HashMap<String, String> departement = new HashMap<>();
         departement.put("parent", ddlDepartements.getSelectedItem().toString());
+        httpRequest = HTTP_REQUEST_PROMOTIONS;
         new HttpRequestManager().execute(departement);
     }
 
@@ -73,24 +79,66 @@ public class SendMessageActivity extends AppCompatActivity implements ListView.O
         @TargetApi(Build.VERSION_CODES.LOLLIPOP)
         @Override
         protected void onPostExecute(JSONObject data) {
-            rdgPromotions.removeAllViews();
-            if(data.names() != null){
+            // Si on cherche à obtenir les promotions d'un DUT.
+            if(httpRequest == HTTP_REQUEST_PROMOTIONS){
+                // La prochaine étape sera la recherche des groupes.
+                httpRequest = HTTP_REQUEST_GROUPES;
+                // On efface les radio butttons existants.
+                rdgPromotions.removeAllViews();
+                // Si il existe des promotions pour ce DUT en BDD.
+                if(data.names() != null){
+                    for(int i=0; i < data.names().length(); i++){
+                        // Création de chaque radio button dynamiquement.
+                        final RadioButton button = new RadioButton(getApplicationContext());
+                        try {
+                            button.setText(data.getJSONObject(data.names().getString(i)).getString("libelle"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        button.setTextColor(Color.BLACK);
+                        ColorStateList colorStateList = new ColorStateList(
+                                new int[][]{
+                                        new int[]{-android.R.attr.state_checked}, // Bouton désactivé
+                                        new int[]{android.R.attr.state_checked} // Bouton activé
+                                }, new int[] {Color.BLACK, Color.BLUE});
+                        button.setButtonTintList(colorStateList);
+                        // Ajout d'un événement permettant de détecter un changement d'état sur chaque radio button.
+                        button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                if (buttonView.isChecked()) {
+                                    // On lance une nouvelle requête pour obtenir les groupes en cas de sélection.
+                                    Log.d("Promotion", buttonView.getText().toString());
+                                    llGroupes.removeAllViews();
+                                    HashMap<String, String> promotion = new HashMap<>();
+                                    promotion.put("parent", buttonView.getText().toString());
+                                    new HttpRequestManager().execute(promotion);
+                                }
+                            }
+                        });
+                        // Ajout de tous les boutons à la vue.
+                        rdgPromotions.addView(button);
+                    }
+                }
+                // Si on cherche les groupes d'une promo.
+            }else if(httpRequest == HTTP_REQUEST_GROUPES){
                 for(int i=0; i < data.names().length(); i++){
-                    RadioButton button = new RadioButton(getApplicationContext());
+                    CheckBox cb = new CheckBox(getApplicationContext());
                     try {
-                        button.setText(data.getJSONObject(data.names().getString(i)).getString("libelle"));
+                        cb.setText(data.getJSONObject(data.names().getString(i)).getString("libelle"));
+                        llGroupes.addView(cb);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    button.setTextColor(Color.BLACK);
+                    cb.setTextColor(Color.BLACK);
                     ColorStateList colorStateList = new ColorStateList(
                             new int[][]{
                                     new int[]{-android.R.attr.state_checked}, // Bouton désactivé
                                     new int[]{android.R.attr.state_checked} // Bouton activé
                             }, new int[] {Color.BLACK, Color.BLUE});
-                    button.setButtonTintList(colorStateList);
-                    rdgPromotions.addView(button);
+                    cb.setButtonTintList(colorStateList);
                 }
+                Log.d("Groupes", data.toString());
             }
         }
     }
